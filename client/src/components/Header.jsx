af7b5f90b8b314f7874c31fb99487676e8a42960
Neha Goel <neha.goel1@indiamart.com>
2024-08-02 12:56:08 +0530

    changes

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,30 +1,66 @@
-import React, { useState } from 'react';
-import { FaPortrait } from 'react-icons/fa';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { FaPortrait } from "react-icons/fa";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
 
 function Header({ loginCred, updateLoginCred }) {
-    
-    const [show, updateShow] = useState(false)
-    // const naviagte = useNavigate()
-    const handleLogout = () => {
-        updateLoginCred({
-            username: '', password: ''
-        })
-        // naviagte("/login");
-    };
-    const showLogoutOption = () => {
-        updateShow(prevState => !prevState)
-    }
+  const [show, updateShow] = useState(false);
+  const handleLogout = () => {
+    updateLoginCred({
+      username: "",
+      password: "",
+    });
+    window.location.reload();
+  };
+  const showLogoutOption = () => {
+    updateShow((prevState) => !prevState);
+  };
 
-    return <div className='Header'>
-        <h1>Survey Creator</h1>
-        {loginCred.username != "" && <div onClick={showLogoutOption} style={{ top: '25px',color: 'white', position: 'absolute', right: '40px' , fontSize: "20px", cursor:"pointer"}}>
-            <FaPortrait />&nbsp;
-            {loginCred.username}
-            {show && <span onClick={handleLogout} className='logout_span'>Logout</span>}
-        </div>}
-        
-    </div>;
+  return (
+    <div
+      className="Header"
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
+    >
+      <h1>Survey Creator</h1>
+      {loginCred.username != "" && (
+        <DropdownButton
+          style={{ marginRight: "2%" }}
+          id="dropdown-basic-button"
+          title={`Hi ${loginCred.username}`}
+        >
+          <Dropdown.Item eventKey="1" onClick={() => handleLogout()}>
+            Logout
+          </Dropdown.Item>
+        </DropdownButton>
+      )}
+      {/* {loginCred.username != "" && (
+        <div
+          onClick={showLogoutOption}
+          style={{
+            top: "25px",
+            color: "white",
+            position: "absolute",
+            right: "40px",
+            fontSize: "20px",
+            cursor: "pointer",
+          }}
+        >
+          <FaPortrait />
+          &nbsp;
+          {loginCred.username}
+          {show && (
+            <span onClick={handleLogout} className="logout_span">
+              Logout
+            </span>
+          )}
+        </div>
+      )} */}
+    </div>
+  );
 }
 
 export default Header;
